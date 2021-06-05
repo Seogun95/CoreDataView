59eaa5b724231778db1119bda869600f1ddfca74
SeonJungKim <ksj083895@gmail.com>
2021-06-05 11:04:16 +0500

    Initial Commit

---
+++ CoreDataView/CoreDataViewApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataViewApp.swift
+//  CoreDataView
+//
+//  Created by Seogun Kim on 2021/06/05.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataViewApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
